cc33b6d4e849f562d191357f1e499db5f49f4fee
adriano <hei.adriano.4@gmail.com>
2023-05-15 23:03:53 +0300

    feat: sign up

--- src/pages/auth/signup.tsx
+++ src/pages/auth/signup.tsx
@@ -1,19 +1,54 @@
-import React from "react";
+import axios from "axios";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
 
 export default function SignUp(){
+    const router = useRouter();
+    const [signUp, setSignUp] = useState({
+    email: "",
+    password: "",
+    name: "",
+    bio: ""
+  });
+
+  async function sendData() {
+    try{
+      const res = await axios.post("http://localhost:3000/users", signUp);
+      console.log(res.data);
+    }
+    catch(error){
+      console.log(error);
+    }
+  };
+
     function handleSubmit(event: React.FormEvent<HTMLFormElement>){
         event.preventDefault();
+        const content = new FormData(event.currentTarget);
+        const emailValue = content.get("email") as string;
+        const passwordValue = content.get("password") as string;
+        const nameValue = content.get("name") as string;
+        const bioValue = content.get("bio") as string;
+
+        setSignUp({
+            ...signUp,
+            email: emailValue,
+            password: passwordValue,
+            name: nameValue,
+            bio: bioValue
+        })
+        sendData();
     }
     return(
         <div className="wrapper fadeInDow">
             <div className="formContent">
                 <h2 className="active">Sign up</h2>
-                <form>
+                <p className="active">Create your own account</p>
+                <form onSubmit={handleSubmit}>
                     <input 
                         type="text"
                         id="email"
                         className="fadeIn second"
-                        name="login"
+                        name="email"
                         placeholder="email"
                     />
                     <input
