378a318946f51ae5a5289ae6fbeba99d2553c71a
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-05-17 23:35:08 +0300

    style: handling error if you don't have a channel

--- src/pages/landing/index.tsx
+++ src/pages/landing/index.tsx
@@ -1,10 +1,41 @@
 import Nav from "@/utils/layout/navbar";
+import HandleError from "@/utils/modals/errorHandling";
+import axios from "axios";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function Home(){
     const router = useRouter();
-    const channel = () => router.push("/landing/channel");
+    const [data, setData] = useState();
+    const [modalOpen, setModalOpen] = useState(false);
+    const close = () => {setModalOpen(false)};
+    const open = () => {setModalOpen(true)};
     const chat = () => router.push("/chat");
+
+    function getData() {
+        event?.preventDefault();
+        axios
+          .get("http://localhost:8080/channels", {
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": sessionStorage.getItem("token")
+            }
+          })
+          .then(response => {
+            if (response.status === 200) {
+              console.log(response);
+              return response.data;
+            } 
+          })
+          .then(data => {
+            setData(data);
+            console.log(data);
+          })
+          .catch(error => {
+            console.error(error)
+            open();
+          });
+      }
     
     return (
         <div>
@@ -27,12 +58,14 @@ export default function Home(){
                         <p className="cookieHeading">Let's chat.</p>
                         <p className="cookieDescription">Already have someone to talk ? ...</p>
                     </div>
-                    <div className="cookieCard" onClick={channel}>
+                    <div className="cookieCard" onClick={getData}>
                         <p className="cookieHeading">Go to channel.</p>
                         <p className="cookieDescription">Share your activity ...</p>
                     </div>
                 </div>
             </div>
+            {modalOpen && <HandleError message={"sorry create a chanel"} handleClose={close} modalOpen={modalOpen}/>}
+            {modalOpen && <button>Create channel</button>}
         </div>
     )
 }
