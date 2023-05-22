901c1ea7f2d9b07d55dbc6f7d702c3461bfc0093
Haritiana Adriano <hei.adriano.4@gmail.com>
2023-05-22 18:55:02 +0300

    chore: use cookie for storing the token

--- src/lib/api/loginApi.ts
+++ src/lib/api/loginApi.ts
@@ -1,27 +1,31 @@
 import axios from "axios";
 import { SetStateAction } from "react";
 import { BACKEND_BASE_URL } from "../../../env";
+import Cookies from "js-cookie";
 
-export function sendLogin(data: any, setModalOpen: React.Dispatch<SetStateAction<boolean>>) {
+export function sendLogin(
+  data: any,
+  setModalOpen: React.Dispatch<SetStateAction<boolean>>
+) {
   axios
     .post(`${BACKEND_BASE_URL}/users/login`, data, {
       headers: {
-        "Content-Type": "application/json"
-      }
+        "Content-Type": "application/json",
+      },
     })
-    .then(response => {
+    .then((response) => {
       if (response.status >= 200 && response.status < 300) {
         console.log(response);
         return response.data;
-      } 
+      }
     })
-    .then(data => {
-      sessionStorage.setItem("token", data.user.token);
-      sessionStorage.setItem("username", data.user.name);
-      sessionStorage.setItem("id", data.user.id);
+    .then((data) => {
+      Cookies.set("token", data.user.token);
+      Cookies.set("username", data.user.name);
+      Cookies.set("id", data.user.id);
       console.log(data.user);
     })
-    .catch(error => {
+    .catch((error) => {
       console.error(error);
       setModalOpen(true);
     });
